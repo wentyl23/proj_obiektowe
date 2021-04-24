4750c581c05817dbb82f5f97430d9f238609c0aa
wentyl23 <mikolaj.mentel98@gmail.com>
2021-04-24 18:52:34 +0200

    full project

---
+++ ex4/src/test/kotlin/MikolajMentel/ex4/Ex4ApplicationTests.kt
@@ -0,0 +1,13 @@
+package MikolajMentel.ex4
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Ex4ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
